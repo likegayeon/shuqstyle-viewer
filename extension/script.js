19cd62896a05b16551a4978acc7c477e433b1d07
likegayeon <blingbling_o@likelion.org>
2018-03-14 11:27:04 +0900

    second feedback edit

--- extension/script.js
+++ extension/script.js
@@ -6,26 +6,23 @@ chrome.contextMenus.create({
         let image_url = encodeURIComponent(info.srcUrl);
         let apiKey = window.localStorage.getItem("apikey");
 
-        alert(apiKey);
-
         chrome.windows.create({
           url: "https://viewer.shuq.com/view.html" + "?type=url&url=" + image_url + "&apikey=" + apiKey,
           type: "popup"
         },  ()=> {
-
         });
       }
 });
 
 // 로컬 스토리지에 key 저장
 $(document).ready(function() {
-    $("input[type=text]").keypress( function(e) {
+    $("#keyInfo").keypress( function(e) {
       if (e.keyCode == 13) {
-          window.localStorage.setItem("apikey", document.getElementById("keyInfo").value);
+          window.localStorage.setItem("apikey", document.querySelector("#keyInfo").value);
       }
     });
   });
 
 // 넣어 놓은 key 값 확인
-var input_key = window.localStorage.getItem("apikey");
-document.getElementById("keyInfo").value = input_key;
+let input_key = window.localStorage.getItem("apikey");
+document.querySelector("#keyInfo").value = input_key;
